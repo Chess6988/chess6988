ddfdc9be83247700a4259fe310b51b7dbd61f745
ANONYMOUS <ANONYMOUS>
2024-08-25 20:02:24 +0200

    i added stars bar

--- lib/Widgets/NewestItemsWidget.dart
+++ lib/Widgets/NewestItemsWidget.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, file_names, sized_box_for_whitespace, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, file_names, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 
 class NewestItemsWidget extends StatelessWidget {
   @override
@@ -46,6 +48,7 @@ class NewestItemsWidget extends StatelessWidget {
                         fontSize: 16, 
                       ),
                       ),
+                      RatingBar.builder(initialRating: 4, minRating: 1, direction: Axis.horizontal, itemCount: 5, itemSize: 18, itemPadding: EdgeInsets.symmetric(horizontal: 4), itemBuilder: (context, _) => Icon(Icons.star), onRatingUpdate: (index) {} ),
                     ],
                   ),
                 ),
